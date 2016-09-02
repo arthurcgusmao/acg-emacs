e51bc57c078b91021d4c23b74ba0f6f8f626dd24
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2016-09-02 10:26:18 -0300

    minor changes

--- acg-editor.el
+++ acg-editor.el
@@ -21,11 +21,16 @@
 ;; makes kill ring not mess with clipboard
 (setq x-select-enable-clipboard nil)
 
+
 ;; store all backup and autosave files in the tmp dir
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
 
 ;; smart tab behavior - indent or complete
 (setq tab-always-indent 'complete)
--- acg-ui.el
+++ acg-ui.el
@@ -2,6 +2,7 @@
 ;; UI, Themes & Cursor
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+(scroll-bar-mode -1)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'zenburn t)
 (setq-default cursor-type 'bar) ;; makes cursor a bar instead of rectangle
@@ -11,23 +12,18 @@
 ;; diminish keeps the modeline tidy
 (require 'diminish)
 
-(beacon-mode +1) ;; never lose the cursor again
+;; never lose the cursor again
+(beacon-mode +1) 
 (diminish 'beacon-mode)
 
-(which-key-mode +1) ;; show available keybindings after you start typing
+;; show available keybindings after you start typing
+(which-key-mode +1)
 (diminish 'which-key-mode)
 
 ;; nice scrolling
 (setq scroll-margin 0
       scroll-conservatively 100000
       scroll-preserve-screen-position 1)
-
-;; remove scroll bars
-;; (setq scroll-bar-mode nil)
-(scroll-bar-mode -1)
-
-
-
 
 
 
