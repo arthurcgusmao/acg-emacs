c7396b75d058dafbba68acbbb4b18507308db0d7
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2016-09-02 10:43:01 -0300

    correction of move-line-up/down

--- modules/move-line-up-or-down.el
+++ modules/move-line-up-or-down.el
@@ -1,17 +1,14 @@
 (defun move-line-down ()
   (interactive)
   (let ((col (current-column)))
-    (save-excursion
-      (forward-line)
-      (transpose-lines 1))
     (forward-line)
+    (transpose-lines 1)
+    (previous-line)
     (move-to-column col)))
 
 (defun move-line-up ()
   (interactive)
   (let ((col (current-column)))
-    (save-excursion
-      (transpose-lines 1))
-    (previous-line)
-    (previous-line)
+    (transpose-lines 1)
+    (previous-line 2)
     (move-to-column col)))
