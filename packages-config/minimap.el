65a472581315530bc56401aa2621b0cb13693cb6
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2016-10-10 23:47:43 -0300

    Configured minimap color and size

--- packages-config/minimap.el
+++ packages-config/minimap.el
@@ -10,5 +10,18 @@
 (add-to-list 'minimap-major-modes 'scss-mode)
 (add-to-list 'minimap-major-modes 'web-mode)
 
+;; changing colors
+(custom-set-faces
+  '(minimap-active-region-background
+    ((((background dark)) (:background "#2A2A2A222222"))
+      (t (:background "#D3D3D3222222")))
+    "Face for the active region in the minimap.
+By default, this is only a different background color."
+    :group 'minimap))
+
+;; minimap size
+(custom-set-variables
+ '(minimap-width-fraction .18))
+
 ;; starts minimap
 (minimap-create)
