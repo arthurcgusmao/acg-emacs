321b79309c0a26d964e6d9f4868820a26f8bf0c3
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2017-04-11 23:21:09 -0300

    feat: custom delete frame function
    Added acg-delete-frame function so now when running
    emacs in the background all buffers are closed when
    the frame is closed.

---
+++ custom-functions/acg-delete-frame.el
@@ -0,0 +1,15 @@
+(defun acg-delete-frame (event)
+  "Kill normal buffers (non-emacs buffers) and delete the current
+frame."
+  (interactive "e")
+  (acg-kill-normal-buffers)
+  (delete-frame)
+  )
+
+(defun acg-kill-normal-buffers ()
+  "Kill all normal buffers (non-emacs buffers)."
+  (dolist (cur (buffer-list))
+    (if (not (equal (substring (buffer-name cur) 0 1) "*"))
+        (kill-buffer cur))))
+
+(define-key special-event-map [delete-frame] 'acg-delete-frame)
--- others/acg-emacs.sh
+++ others/acg-emacs.sh
@@ -2,10 +2,7 @@
 
 emacsclient -n -e "(null (cl-remove-if (lambda (frame) (null (display-graphic-p frame))) (frame-list)))" | grep >/dev/null nil
 if [ "$?" = "1" ]; then
-    # emacsclient -c -n -a "" "$@"
-    emacs "$@"
+    emacsclient -c -n -a "" "$@"
 else
     emacsclient -n -a "" "$@"
 fi
-
-#emacsclient -a "emacs" "$@" &
