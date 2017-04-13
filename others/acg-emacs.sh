57ece571454afb9fec0c05dde3c017e28ba951dc
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2017-04-13 00:06:28 -0300

    fix: acg-emacs.sh not bringing to front
    Shell script acg-emacs wasn't bringing the window to front in OS, so this fixed it.

--- others/acg-emacs.sh
+++ others/acg-emacs.sh
@@ -2,7 +2,11 @@
 
 emacsclient -n -e "(null (cl-remove-if (lambda (frame) (null (display-graphic-p frame))) (frame-list)))" | grep >/dev/null nil
 if [ "$?" = "1" ]; then
+    # no frame open
     emacsclient -c -n -a "" "$@"
 else
     emacsclient -n -a "" "$@"
 fi
+
+# bring application to front
+emacsclient -e '(select-frame-set-input-focus (selected-frame))'
