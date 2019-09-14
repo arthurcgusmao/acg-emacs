94f1d3e525915042955653fb1a1924bc187404fc
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2019-09-14 18:49:02 +0200

    elisp mode settings

--- major-modes-config/acg-elisp.el
+++ major-modes-config/acg-elisp.el
@@ -1,2 +1,10 @@
-(define-key emacs-lisp-mode-map (kbd "<f7>") (crux-with-region-or-line eval-region))
+
+(defalias 'eval-region-and-print
+  (lambda (START END) (eval-region START END t))
+  "Same as eval-region but prints output to the messages
+  buffer.")
+
+;; keybindings
+(define-key emacs-lisp-mode-map (kbd "<f6>") 'eval-expression)
+(define-key emacs-lisp-mode-map (kbd "<f7>") (crux-with-region-or-line eval-region-and-print))
 (define-key emacs-lisp-mode-map (kbd "<f8>") 'eval-buffer)
