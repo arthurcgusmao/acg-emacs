8a13f57aa98f820396bb1e97fb7a71156ce18c73
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2019-10-16 22:23:54 +0200

    yasnippet configs
    Added ivy-yasnippet package and lorem ipsum for text-mode. Binded
    C-c <tab> to IVY-YASNIPPET function.

--- minor-modes-config/acg-yasnippet.el
+++ minor-modes-config/acg-yasnippet.el
@@ -1,6 +1,10 @@
 (require 'yasnippet)
+(require 'ivy-yasnippet)
 
 (add-to-list 'yas-snippet-dirs
              (concat acg-emacs-dir "snippets"))
 
 (yas-global-mode t)
+
+;; keybindings
+(define-key yas-minor-mode-map (kbd "C-c <tab>") 'ivy-yasnippet)
