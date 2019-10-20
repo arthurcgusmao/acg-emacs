3293df3eca36baba2c89125129c33e2ff618088e
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2019-10-21 01:30:15 +0200

    dired: refactor, sidebar, use same buffer
    Refactored dired file to use `use-package`. `dired-sidebar` package was
    included, without customizations (to be done later). Dired custom
    functions were created/binded to "reuse" the same dired buffer when
    opening files or directories.

--- major-modes-config/acg-dired.el
+++ major-modes-config/acg-dired.el
@@ -1,8 +1,53 @@
-(require 'dired-subtree)
+(use-package dired
+  :config
+  (put 'dired-find-alternate-file 'disabled nil) ;; enable disabled command
+  
+  (defun acg/dired-alternate-up-directory (&optional other-window)
+    "Run Dired on parent directory of current directory.
+Taken from https://www.emacswiki.org/emacs/DiredReuseDirectoryBuffer"
+    (interactive "P")
+    (let* ((dir (dired-current-directory))
+           (orig (current-buffer))
+           (up (file-name-directory (directory-file-name dir))))
+      (or (dired-goto-file (directory-file-name dir))
+          ;; Only try dired-goto-subdir if buffer has more than one dir.
+          (and (cdr dired-subdir-alist)
+               (dired-goto-subdir up))
+          (progn
+            (kill-buffer orig)
+            (dired up)
+            (dired-goto-file dir)))))
 
-(global-set-key (kbd "C-x d") 'dired-jump)
-(define-key dired-mode-map (kbd "C-o") nil)
-(define-key dired-mode-map (kbd "<backspace>") 'dired-up-directory)
-(define-key dired-mode-map (kbd "<tab>") 'dired-subtree-cycle)
-(define-key dired-mode-map (kbd "<S-iso-lefttab>") 'dired-subtree-remove)
-(define-key dired-mode-map (kbd "q") 'kill-current-buffer)
+  (defun acg/dired-mouse-find-alternate-file (event &optional find-dir-func)
+    "Calls `dired-mouse-find-file' with FIND-FILE-FUNC equals to
+    `find-alternate-file'."
+    (interactive "e")
+     (let ((orig (current-buffer)))
+       (dired-mouse-find-file event #'find-alternate-file find-dir-func)
+       (kill-buffer orig)))
+  :bind
+  (("C-x d" . dired-jump)
+   :map dired-mode-map
+   ("C-o" . nil)
+   ("q" . 'kill-current-buffer)
+   ("<return>" . 'dired-find-alternate-file)
+   ("<C-return>" . 'dired-find-file)
+   ("<backspace>" . 'acg/dired-alternate-up-directory)
+   ("<C-backspace>" . 'dired-up-directory)
+   ([C-mouse-2] . 'dired-mouse-find-file-other-window) ;; not working @todo
+   ([mouse-2] . 'acg/dired-mouse-find-alternate-file)))
+
+(use-package dired-subtree
+  :ensure t
+  :after dired
+  :bind
+  (:map dired-mode-map
+   ("<tab>" . 'dired-subtree-cycle)
+   ("<S-iso-lefttab>" . 'dired-subtree-remove)))
+
+(use-package dired-sidebar
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar))
+
+
+
