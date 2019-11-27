d4ad7c0d6502d39609e0ae420f3a282e5143499b
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2019-11-27 13:58:20 +0100

    Dynamic WSL frame font
    Added code to automatically adjust the frame font depending on the
    screen resolution.

--- others/wsl/acg-wsl-adaptations.el
+++ others/wsl/acg-wsl-adaptations.el
@@ -3,7 +3,10 @@
 ;; configure font. For whatever reason Emacs does not start with XFCE4's
 ;; default monospace font
 (defun fontify-frame-wsl (frame)
-  (set-frame-parameter frame 'font "Hack-9"))
+  (cond
+   ((> (display-pixel-height) 2700) (set-frame-parameter frame 'font "Hack-15"))
+   ((> (display-pixel-height) 2000) (set-frame-parameter frame 'font "Hack-11.5"))
+   (t (set-frame-parameter frame 'font "Hack-9"))))
 
 (fontify-frame-wsl nil) ;; Fontify current frame
 (push 'fontify-frame-wsl after-make-frame-functions) ;; Fontify any future frames
