8caf6088afa5ca2ecd242754c8754a7b36ef2f28
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2020-03-11 18:45:03 +0100

    Increase/Decrease whole frame font
    See https://stackoverflow.com/a/60641769/5103881

--- acg-ui.el
+++ acg-ui.el
@@ -181,6 +181,8 @@ thresholds are not met."
 
 
 
+;; Font configurations
+
 ;; Configure font in MS-Windows
 (defun acg/fontify-frame-mswindows (frame)
   (set-frame-parameter frame 'font "Consolas-10"))
@@ -210,6 +212,31 @@ which is when we want to apply these configurations
 
 (acg/set-custom-frame-font (selected-frame)) ; Conditionally fontify current frame, if any
 (add-to-list 'after-make-frame-functions #'acg/set-custom-frame-font) ; Conditionally fontify any future frames
+
+
+;; Resize the whole frame, and not only a window
+;; Adapted from https://stackoverflow.com/a/24714383/5103881
+(defun acg/zoom-frame (&optional amt frame)
+  "Increaze FRAME font size by amount AMT. Defaults to selected
+frame if FRAME is nil, and to 1 if AMT is nil."
+  (interactive "p")
+  (let* ((frame (or frame (selected-frame)))
+         (font (face-attribute 'default :font frame))
+         (size (font-get font :size))
+         (amt (or amt 1))
+         (new-size (+ size amt)))
+    (set-frame-font (font-spec :size new-size) t `(,frame))
+    (message "Frame's font new size: %d" new-size)))
+
+(defun acg/zoom-frame-out (&optional amt frame)
+  "Call `acg/zoom-frame' with negative argument."
+  (interactive "p")
+  (acg/zoom-frame (- (or amt 1)) frame))
+
+(global-set-key (kbd "C-x C-=") 'acg/zoom-frame)
+(global-set-key (kbd "C-x C--") 'acg/zoom-frame-out)
+(global-set-key (kbd "<C-down-mouse-4>") 'acg/zoom-frame)
+(global-set-key (kbd "<C-down-mouse-5>") 'acg/zoom-frame-out)
 
 
 (provide 'acg-ui)
