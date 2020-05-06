e125807cc191126e949c11137ae6fa16fa0fadae
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2020-05-06 23:25:50 +0200

    New projectiny bindings

--- acg-emacs/minor-modes-config/acg-project.el
+++ acg-emacs/minor-modes-config/acg-project.el
@@ -1,9 +1,12 @@
-(use-package project
-  :straight nil
-  :bind (("M-o" . project-find-file)))
+;; (use-package project
+;;   :straight nil
+;;   :bind (("M-o" . project-find-file)))
 
 (use-package projectiny
+  :after project
   :straight (:host github :repo "arthurcgusmao/projectiny")
   :bind
-  (("C-c p" . projectiny-find-file-in)
-   ("M-O" . projectiny-find-file-all)))
+  (("M-o" . projectiny-find-file)
+   ("M-O" . projectiny-find-file-all)
+   ("C-M-o" . projectiny-find-file-in)
+   ("C-c p" . projectiny-find-file-in)))
