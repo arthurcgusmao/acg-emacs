aa17eff4ca0d792172fb64774f2461092bfddaba
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2020-05-13 02:11:23 +0200

    Magit: refactor configs to use-package

--- acg-emacs/major-modes-config/acg-magit.el
+++ acg-emacs/major-modes-config/acg-magit.el
@@ -1,59 +1,36 @@
-(use-package magit)
+(use-package magit
+  :config
+  ;; Disable Magit asking to save files
+  (setq magit-save-repository-buffers nil)
 
-;; Disable Magit asking to save files
-(setq magit-save-repository-buffers nil)
+  ;; Custom functions for visiting files
 
-;; Custom functions for visiting files
-
-(defun acg/magit-diff-visit-file (file)
-  "Same as `magit-diff-visit-file', but uses the default
+  (defun acg/magit-diff-visit-file (file)
+    "Same as `magit-diff-visit-file', but uses the default
 `pop-to-buffer' (which in turn uses `display-buffer') to display
 the respective file."
-  (interactive (list (magit-file-at-point t t)))
-  (magit-diff-visit-file--internal file nil #'pop-to-buffer))
+    (interactive (list (magit-file-at-point t t)))
+    (magit-diff-visit-file--internal file nil #'pop-to-buffer))
 
-(defun acg/magit-diff-display-file (file)
-  "Display Magit's file at point in some buffer (uses the default
-`display-buffer')"
-  (interactive (list (magit-file-at-point t t)))
-  (magit-diff-visit-file--internal file nil #'display-buffer))
+  (defun acg/magit-diff-display-file (file)
+    "Display Magit's file at point in some buffer (uses the
+default `display-buffer')"
+    (interactive (list (magit-file-at-point t t)))
+    (magit-diff-visit-file--internal file nil #'display-buffer))
 
-(defun acg/magit-diff-display-file-and-next (file)
-  "Same as `acg/magit-diff-display-file', but moves to the next
+  (defun acg/magit-diff-display-file-and-next (file)
+    "Same as `acg/magit-diff-display-file', but moves to the next
 file before displaying file at point."
-  (interactive (list (magit-file-at-point t t)))
-  (magit-next-line)
-  (magit-diff-visit-file--internal file nil #'display-buffer))
+    (interactive (list (magit-file-at-point t t)))
+    (magit-next-line)
+    (magit-diff-visit-file--internal file nil #'display-buffer))
 
-(defun acg/magit-diff-display-file-and-previous (file)
-  "Same as `acg/magit-diff-display-file', but moves to the
+  (defun acg/magit-diff-display-file-and-previous (file)
+    "Same as `acg/magit-diff-display-file', but moves to the
 previous file before displaying file at point."
-  (interactive (list (magit-file-at-point t t)))
-  (magit-previous-line)
-  (magit-diff-visit-file--internal file nil #'display-buffer))
-
-
-(with-eval-after-load 'magit
-  (global-set-key (kbd "C-x g") 'magit-status)
-  (define-key magit-status-mode-map (kbd "<C-tab>") nil)
-  (define-key magit-status-mode-map (kbd "C-c <tab>") 'magit-section-cycle)
-  (define-key magit-process-mode-map (kbd "<C-tab>") nil)
-  (define-key magit-process-mode-map (kbd "C-c <tab>") 'magit-section-cycle)
-  (define-key magit-log-mode-map (kbd "<C-tab>") nil)
-  (define-key magit-log-mode-map (kbd "C-c <tab>") 'magit-section-cycle)
-
-  (define-key magit-mode-map [escape] 'keyboard-quit)
-  (define-key magit-log-mode-map [escape] 'magit-kill-this-buffer)
-  (define-key magit-mode-map (kbd "C-w") 'magit-kill-this-buffer)
-  (define-key magit-log-mode-map (kbd "C-w") 'magit-kill-this-buffer)
-  (define-key magit-mode-map (kbd "q") 'magit-kill-this-buffer)
-  (define-key magit-log-mode-map (kbd "q") 'magit-kill-this-buffer)
-
-  (define-key with-editor-mode-map (kbd "C-w") 'with-editor-cancel)
-  (define-key with-editor-mode-map (kbd "C-s") 'with-editor-finish)
-
-  (define-key magit-mode-map (kbd "<C-up>") 'magit-section-backward-sibling)
-  (define-key magit-mode-map (kbd "<C-down>") 'magit-section-forward-sibling)
+    (interactive (list (magit-file-at-point t t)))
+    (magit-previous-line)
+    (magit-diff-visit-file--internal file nil #'display-buffer))
 
   (mapc               ; Programatically remap M-[1-4] for all desired mode-maps
    (lambda (mode-map)
@@ -67,29 +44,42 @@ previous file before displaying file at point."
      (define-key (symbol-value mode-map) (kbd "C-c 4") 'magit-section-show-level-4-all))
    '(magit-status-mode-map magit-diff-mode-map))
 
-  (define-key magit-file-section-map (kbd "<return>") 'acg/magit-diff-visit-file)
-  (define-key magit-file-section-map (kbd "<C-return>") 'acg/magit-diff-display-file)
-  (define-key magit-file-section-map (kbd "<S-return>") 'acg/magit-diff-display-file-and-next)
-  (define-key magit-file-section-map (kbd "<C-S-return>") 'acg/magit-diff-display-file-and-previous)
-  )
+  :bind
+  (("C-x g" . magit-status)
+   :map magit-status-mode-map
+   ("<C-tab>" . nil)
+   ("C-c <tab>" . magit-section-cycle)
+   :map magit-process-mode-map
+   ("<C-tab>" . nil)
+   ("C-c <tab>" . magit-section-cycle)
+   :map magit-log-mode-map
+   ([escape] . magit-kill-this-buffer)
+   ("C-w" . magit-kill-this-buffer)
+   ("q" . magit-kill-this-buffer)
+   ("<C-tab>" . nil)
+   ("C-c <tab>" . magit-section-cycle)
+   :map magit-mode-map
+   ([escape] . keyboard-quit)
+   ("C-w" . magit-kill-this-buffer)
+   ("q" . magit-kill-this-buffer)
+   ("<C-up>" . magit-section-backward-sibling)
+   ("<C-down>" . magit-section-forward-sibling)
+   :map magit-file-section-map
+   ("<return>" . acg/magit-diff-visit-file)
+   ("<C-return>" . acg/magit-diff-display-file)
+   ("<S-return>" . acg/magit-diff-display-file-and-next)
+   ("<C-S-return>" . acg/magit-diff-display-file-and-previous)))
 
+(use-package with-editor
+  :bind
+  (:map with-editor-mode-map
+        ("C-w" . with-editor-cancel)
+        ("C-s" . with-editor-finish)))
 
-(use-package transient)
-
-(with-eval-after-load 'transient
-  (define-key transient-map (kbd "<escape>") 'transient-quit-all)
-  (define-key transient-map (kbd "<escape>") 'transient-quit-all)
-  )
-
-
-;; (global-set-key (kbd "C-x g") 'magit-status)
-
-;; (define-key magit-popup-mode-map [escape] 'magit-popup-quit)
-;; (define-key magit-mode-map [escape] 'magit-mode-bury-buffer)
-;; (define-key magit-log-mode-map [escape] 'magit-log-bury-buffer)
-
-;; (define-key magit-mode-map (kbd "C-w") 'magit-kill-this-buffer)
-
+(use-package transient
+  :bind
+  (:map transient-map
+        ("<escape>" . transient-quit-all)))
 
 
 ;; Provide commit message guidelines/feedback when committing;
