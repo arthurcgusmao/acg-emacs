1c8f330a41b5db2cf6cd38707cf27fc45f6de634
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2020-05-30 03:07:22 +0200

    Custom version of `open-line` function

--- acg-emacs/minor-modes-config/acg-crux.el
+++ acg-emacs/minor-modes-config/acg-crux.el
@@ -1,9 +1,23 @@
 (use-package crux
   :config
   (crux-reopen-as-root-mode 1)
+
+  (defun acg/open-line ()
+    "Alternative version of `open-line' that indents the newly
+created line."
+    (interactive)
+    ;; @todo: add wrapper to handle execution as single "undo"
+    (newline)
+    (newline)
+    (previous-line)
+    (indent-according-to-mode)
+    (delete-char 1)
+    (previous-line)
+    (end-of-line))
+
   :bind
   (("<C-return>" . crux-smart-open-line)
    ("<C-S-return>" . crux-smart-open-line-above)
    ;; ("<M-return>" . indent-new-comment-line)
-   ("<M-return>" . open-line)
+   ("<M-return>" . acg/open-line)
    ("C-j" . crux-top-join-line)))
