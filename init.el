b023acfb541324faa142cc8f349f0f751b4d75fc
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2020-07-07 17:42:01 +0200

    Option to have a local-settings file
    Added the automatic load, without error if it doesn't exist, of a file
    for storing local settings. This might be used in case you have some
    particular setting in a workspace that only applies there.

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
 ;; loads acg-emacs
 (add-to-list 'load-path (concat user-emacs-directory "acg-emacs"))
 (require 'acg-core)
+
+;; Use `local-settings.el' file for any local configurations you might wanna have
+(load (concat user-emacs-directory "local-settings.el") t)
