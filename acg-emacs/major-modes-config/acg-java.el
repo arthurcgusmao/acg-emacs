75ade2bfc0c06d795559f0d206055e4c1ca57cb0
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2020-09-24 01:23:28 +0200

    Rainbow identifiers + Placeholder for Java configs

---
+++ acg-emacs/major-modes-config/acg-java.el
@@ -0,0 +1,6 @@
+;; References:
+;; - http://www.goldsborough.me/emacs,/java/2016/02/24/22-54-16-setting_up_emacs_for_java_development/
+;; - https://blog.jmibanez.com/2019/03/31/emacs-as-java-ide-revisited.html
+
+;; My notes:
+;; - lsp-java seems the best option -- second link has configs for it
---
+++ acg-emacs/minor-modes-config/acg-misc-minor-modes.el
@@ -0,0 +1,2 @@
+;; Rainbow identifiers colors syntax elements of the code in unique colors
+(use-package rainbow-identifiers)
