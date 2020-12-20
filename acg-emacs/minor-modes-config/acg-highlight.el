0c79a93c0c0684009530a25dac6525e0f3e980b4
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2020-12-20 13:49:10 -0300

    Add goggles mode (highlight pasted region)
    Added goggles, a minor mode that highlights regions of the buffer that
    are being pasted, deleted/killed, undone/redone, etc.

--- acg-emacs/minor-modes-config/acg-highlight.el
+++
@@ -1,8 +0,0 @@
-(use-package highlight)
-(global-unset-key (kbd "M-h"))
-;; changes the keymap to M-h
-(global-set-key (kbd "M-h") hlt-map)
-;; add custom keymaps that I find easier to remember
-(global-set-key (kbd "M-h u a") 'hlt-unhighlight-all-prop)
-
-;; @todo: Switch workflow to use hi-lock
