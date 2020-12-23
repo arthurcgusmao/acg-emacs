a44a4e9b1219281bbc1432d09beec3f07d745a5c
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2020-12-23 14:38:37 -0300

    Goggles: config face colors
    Configured the colors of the goggles package to be more 'pure' colors
    that look better and should work better across different themes.

--- acg-emacs/minor-modes-config/acg-highlighting.el
+++ acg-emacs/minor-modes-config/acg-highlighting.el
@@ -15,5 +15,12 @@
 (use-package goggles
   :straight (:host github :repo "minad/goggles")
   :config
-  (goggles-mode)
-  (setq goggles-pulse t)) ;; keep highlighted or just pulse highlight
+  ;; Face colors
+  (set-face-background 'goggles-changed "yellow")
+  (set-face-background 'goggles-removed "red")
+  (set-face-background 'goggles-added "green")
+  ;; Timing configs
+  (setq goggles-pulse-iterations 10)
+  (setq goggles-pulse-delay 0.03)
+  (setq goggles-pulse t) ;; keep highlighted or just pulse highlight
+  (goggles-mode +1))
