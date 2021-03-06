b720ae82e6772d31fcda3e318c9433de59d8c8d9
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2021-03-06 20:53:19 +0100

    Undo-tree: update custom funcs and make ESC abort
    Updated the custom functions I had created previously with their actual
    behavior description. My previous modifications had a side effect I did
    not notice: they were not only keeping the previous region state but
    also not moving the cursor when undoing, which is undesired.
    
    Also, modified the binding of the ESC key to quit and abort the
    undo-tree-visualizer (making the buffer go back to its original state --
    when the user started the visualizer in the first place).

--- acg-emacs/minor-modes-config/acg-undo-tree.el
+++ acg-emacs/minor-modes-config/acg-undo-tree.el
@@ -9,14 +9,16 @@
 
   ;; Custom commands to preserve active region when undoing
   (defun acg/undo-tree-undo (&optional arg)
-    "Same as `undo-tree-undo' but does not deactivate selected region."
+    "Same as `undo-tree-undo' but does not deactivate selected
+region nor moves the cursor position."
     (interactive "*P")
     (save-excursion
       (undo-tree-undo arg)
       (setq deactivate-mark nil)))
 
   (defun acg/undo-tree-redo (&optional arg)
-    "Same as `undo-tree-redo' but does not deactivate selected region."
+    "Same as `undo-tree-redo' but does not deactivate selected
+region nor moves the cursor position."
     (interactive "*P")
     (save-excursion
       (undo-tree-redo arg)
@@ -28,8 +30,8 @@
   (:map undo-tree-map
    ("C-/" . nil)
    ("C-?" . nil)
-   ("C-z" . acg/undo-tree-undo)
-   ("C-S-z" . acg/undo-tree-redo)
+   ("C-z" . undo-tree-undo)
+   ("C-S-z" . undo-tree-redo)
    :map undo-tree-visualizer-mode-map
    ("RET" . undo-tree-visualizer-quit)
-   ([escape] . undo-tree-visualizer-quit)))
+   ([escape] . undo-tree-visualizer-abort)))
