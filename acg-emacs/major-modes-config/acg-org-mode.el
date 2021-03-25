78ac39955624424d27c735f066fc1d6abdeb094e
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2021-03-25 01:57:01 +0100

    Org: add package org-yt -- http images inline
    Added the package `org-yt` which allows to display images in a remote
    URL inline as if they were present locally.

--- acg-emacs/major-modes-config/acg-org-mode.el
+++ acg-emacs/major-modes-config/acg-org-mode.el
@@ -431,6 +431,32 @@ active."
    ("C-c i r" . org-download-rename-at-point)
    ("C-c i s" . org-download-screenshot)))
 
+;; Org YouTube makes it possible to exhibit YouTube and Images from the web
+;; with `org-toggle-inline-images'.
+(use-package org-yt
+  :straight (:host github :repo "TobiasZawada/org-yt")
+  :config
+  ;; Make external images work in Org mode
+  (defun org-image-link (protocol link _description)
+    "Interpret LINK as base64-encoded image data."
+    (cl-assert (string-match "\\`img" protocol) nil
+               "Expected protocol type starting with img")
+    (let ((buf (url-retrieve-synchronously (concat (substring protocol 3) ":" link))))
+      (cl-assert buf nil
+                 "Download of image \"%s\" failed." link)
+      (with-current-buffer buf
+        (goto-char (point-min))
+        (re-search-forward "\r?\n\r?\n")
+        (buffer-substring-no-properties (point) (point-max)))))
+
+  (org-link-set-parameters
+   "imghttp"
+   :image-data-fun #'org-image-link)
+
+  (org-link-set-parameters
+   "imghttps"
+   :image-data-fun #'org-image-link))
+
 
 ;; Org SRC blocks
 
