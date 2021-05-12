e64afc9bccc562fd0b006980b617ffca8b415b75
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2021-05-12 14:18:58 +0200

    Make `C-c ESC` quit command sequence

--- acg-emacs/custom-functions/acg-super-keyboard-quit.el
+++ acg-emacs/custom-functions/acg-super-keyboard-quit.el
@@ -3,12 +3,18 @@
   (interactive)
   (keyboard-quit)
   (abort-recursive-edit)
-  (setq quit-flag t)
-  )
+  (setq quit-flag t))
 
-;; keybindings
+(defun acg/dummy-quit ()
+  "Function that mimics quit to use when canceling a keyboard
+sequence."
+  (interactive)
+  (message "Quit"))
 
-;;; esc always quits
+
+;;; Keybindings
+
+;; ESC always quits
 (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
@@ -16,6 +22,7 @@
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 (eval-after-load "anaconda-mode-view-mode" '(define-key anaconda-mode-view-mode-map [escape] 'kill-buffer-and-window))
 (global-set-key [escape] 'acg/super-keyboard-quit)
+(acg/force-global-set-key "C-c <escape>" 'acg/dummy-quit)
 ;; rebind ESC functions to C-<escape>
 (define-key key-translation-map (kbd "C-<escape>") (kbd "ESC"))
 
