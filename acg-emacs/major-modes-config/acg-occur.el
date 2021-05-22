f71b6af84ae0f29535b8dbfc1ca02429b19fe202
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2021-05-22 12:58:02 +0200

    Occur-mode: display, display-and-next functions
    Added the same functions and keybindings that I have for dired mode, to
    be able to "select" an occur occurrence but continue on the occur buffer
    with <C-return>. The option to move to the next/previous occurrences can
    be performed with <S-return> and <C-return>, respectively.

--- acg-emacs/major-modes-config/acg-occur.el
+++ acg-emacs/major-modes-config/acg-occur.el
@@ -1,13 +1,40 @@
-(defun occur-kill-line()
-  "Quick and dirty discard line from occur resultset.
-from https://emacs.stackexchange.com/a/52865/13589"
-  (interactive)
-  (let ((inhibit-read-only t))
-    (kill-whole-line)
-    (delete-blank-lines)))
+(use-package replace
+  :straight nil
+  :config
 
-;; switch to occur buffer automatically after running occur
-(add-hook 'occur-hook (lambda () (switch-to-buffer-other-window "*Occur*")))
+  ;; Switch to occur buffer automatically after running occur
+  (add-hook 'occur-hook (lambda () (switch-to-buffer-other-window "*Occur*")))
 
-;; keybindings
-(define-key occur-mode-map (kbd "d") 'occur-kill-line)
+
+  ;; Custom functions
+
+  (defun acg/occur-kill-line ()
+    "Quick and dirty discard line from occur resultset.
+From https://emacs.stackexchange.com/a/52865/13589"
+    (interactive)
+    (let ((inhibit-read-only t))
+      (kill-whole-line)
+      (delete-blank-lines)))
+
+  (defun acg/occur-mode-display-occurrence-and-next ()
+    "Same as `occur-mode-display-occurrence' but jumps to next
+line afterwards."
+    (interactive)
+    (occur-mode-display-occurrence)
+    (next-line))
+
+  (defun acg/occur-mode-display-occurrence-and-previous ()
+    "Same as `occur-mode-display-occurrence' but jumps to previous
+line afterwards."
+    (interactive)
+    (occur-mode-display-occurrence)
+    (previous-line))
+
+
+  :bind
+  (:map occur-mode-map
+        ("d" . acg/occur-kill-line)
+        ("<C-return>" . occur-mode-display-occurrence)
+        ("<S-return>" . acg/occur-mode-display-occurrence-and-next)
+        ("<C-S-return>" . acg/occur-mode-display-occurrence-and-previous)
+        ("C-o" . nil)))
