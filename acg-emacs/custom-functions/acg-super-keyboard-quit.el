5b6cc26f75d56867f4bae65b5a38c5632eb3ac40
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2021-05-23 02:55:31 +0200

    Minibuffer: always quit (never deselect text)

--- acg-emacs/custom-functions/acg-super-keyboard-quit.el
+++ acg-emacs/custom-functions/acg-super-keyboard-quit.el
@@ -14,16 +14,19 @@ sequence."
 
 ;;; Keybindings
 
-;; ESC always quits
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+;;;; ESC always quits
+;; For the minibuffer, deliberately use `abort-recursive-edit' instead of
+;; `minibuffer-keyboard-quit' because the latter will not quit when text is
+;; selected in the minibuffer.
+(define-key minibuffer-local-map [escape] 'abort-recursive-edit)
+(define-key minibuffer-local-ns-map [escape] 'abort-recursive-edit)
+(define-key minibuffer-local-completion-map [escape] 'abort-recursive-edit)
+(define-key minibuffer-local-must-match-map [escape] 'abort-recursive-edit)
+(define-key minibuffer-local-isearch-map [escape] 'abort-recursive-edit)
 (eval-after-load "anaconda-mode-view-mode" '(define-key anaconda-mode-view-mode-map [escape] 'kill-buffer-and-window))
 (global-set-key [escape] 'acg/super-keyboard-quit)
 (acg/force-global-set-key "C-c <escape>" 'acg/dummy-quit)
-;; rebind ESC functions to C-<escape>
+;; Rebind ESC functions to C-<escape>
 (define-key key-translation-map (kbd "C-<escape>") (kbd "ESC"))
 
 (use-package help-mode
