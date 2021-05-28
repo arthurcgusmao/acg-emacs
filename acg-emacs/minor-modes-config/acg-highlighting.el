a3ae8d8b4b7675ad3045276c754d1f701ab4c43f
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2021-05-28 15:51:12 +0200

    Remove highlight package (use built-in hi-lock)

--- acg-emacs/minor-modes-config/acg-highlighting.el
+++ acg-emacs/minor-modes-config/acg-highlighting.el
@@ -1,16 +1,5 @@
 ;;; Packages for highlighting the buffer in smart ways
 
-
-(use-package highlight)
-(global-unset-key (kbd "M-h"))
-;; changes the keymap to M-h
-(global-set-key (kbd "M-h") hlt-map)
-;; add custom keymaps that I find easier to remember
-(global-set-key (kbd "M-h u a") 'hlt-unhighlight-all-prop)
-
-;; @todo: Switch workflow to use hi-lock
-
-
 ;; Highlight region when pasting, undoing/redoing, killing/deleting
 (use-package goggles
   :straight (:host github :repo "minad/goggles")
