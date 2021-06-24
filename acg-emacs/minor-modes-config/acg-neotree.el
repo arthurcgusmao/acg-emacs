16cddca31cb5305831dc1d80ef0f19086bdd72d4
Arthur Colombini Gusmão <arthurcgusmao@users.noreply.github.com>
2021-06-24 14:33:39 +0200

    Neotree: allow resizing neotree window

--- acg-emacs/minor-modes-config/acg-neotree.el
+++ acg-emacs/minor-modes-config/acg-neotree.el
@@ -1,3 +1,6 @@
-(use-package neotree)
-(global-set-key (kbd "M-d") 'neotree-find)
-(global-set-key (kbd "M-D") 'neotree-hide)
+(use-package neotree
+  :config
+  (setq neo-window-fixed-size nil)      ; Allow resizing neotree window
+  :bind
+  ("M-d" . neotree-find)
+  ("M-D" . neotree-hide))
