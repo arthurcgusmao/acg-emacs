0a99dc2926a2a740819cf6e7ef92a8c6cea97ba2
Arthur Colombini Gusmão <arthurcgusmao@users.noreply.github.com>
2022-01-30 11:21:37 +0100

    Adjust unmodified-buffer settings

--- acg-emacs/custom-functions/acg-update-unmodified-buffer.el
+++ acg-emacs/custom-functions/acg-update-unmodified-buffer.el
@@ -1,4 +1,4 @@
 (use-package unmodified-buffer
   :straight (:host github :protocol ssh
                    :repo "arthurcgusmao/unmodified-buffer")
-  :hook (after-init . unmodified-buffer-mode))
+  :hook (after-init . unmodified-buffer-global-mode))
